518c22dbc6ac79a19a3d938c83a0edc6f2842a9b
Pujon Das Auvi <auvipujondas@gmail.com>
2024-12-15 14:05:32 +0600

    redis utils added

---
+++ src/app/utils/redis.utils.ts
@@ -0,0 +1,33 @@
+import redisClient from "../config/redis.config"
+
+export const cacheData = async (key: string, data: any, expirationTime: number) => {
+    try{
+        await redisClient.setEx(key, expirationTime, JSON.stringify(data));
+        console.log('Data cached successfully');
+    } catch (error) {
+        console.error('Error caching data:', error);
+    }
+}
+
+export const getCachedData = async (key: string) => {
+    try{
+        const data = await redisClient.get(key);
+        if (data) {
+            return JSON.parse(data);
+        }
+        return null;
+    } catch (error) {
+        console.error('Error getting cached data:', error);
+        return null;
+    }
+}
+
+export const deleteCachedData = async (key: string) => {
+    try{
+        await redisClient.del(key);
+        console.log('Cached data deleted successfully');
+    } catch (error) {
+        console.error('Error deleting cached data:', error);
+    }
+}
+
