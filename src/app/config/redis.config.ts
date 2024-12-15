f452bf3b3056a48df3b613f9b27f1e03c1916c25
Pujon Das Auvi <auvipujondas@gmail.com>
2024-12-15 14:01:14 +0600

    redis config done

---
+++ src/app/config/redis.config.ts
@@ -0,0 +1,28 @@
+import { createClient } from "redis";
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL || "redis://localhost:6379",
+    socket: {
+        reconnectStrategy: (retries) => {
+            if (retries >= 3) {
+                return new Error("Failed to connect to Redis");
+            }
+            return Math.min(retries * 50, 2000);
+        }
+    }
+})
+
+redisClient.on('error', (err) => {
+    console.error('Redis error:', err);
+});
+
+redisClient.on('connect', () => {
+    console.log('Connected to Redis');
+});
+redisClient.on('ready', () => console.log('Redis Client Ready'))
+redisClient.on('reconnecting', () => console.log('Redis Client Reconnecting'))
+redisClient.on('end', () => console.log('Redis Client Ended'))
+
+redisClient.connect().catch(err => console.error('Redis Connection Error:', err));
+
+export default redisClient;
